122ccab6f15a0566370f0b8d7b74a5a0a8398e77
EvolutecnetDev2 <83365151+EvolutecnetDev2@users.noreply.github.com>
2022-04-20 12:37:44 -0300

    Inserir uma novo site (iMov)
    primeira página do Imov

---
+++ Documento/novo_pg.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> nova página do site</title>
+    <link rel="stylesheet" type="text/css" href="novo_pg.css">
+    <link href="../bootstrap/css/bootstrap-class.css" rel="stylesheet" type="text/css">
+    <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <link href="../bootstrap/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<body>
+
+<main>
+
+    <div class="vidro">
+        <div id="perfil">
+            <div id="Nperfil">
+                <h1>perfil</h1></div>
+            <img src="./avatar.png" id="imgperfil"/>
+        </div>
+        <div id="titulo">
+            <h1>Disney</h1>
+        </div>
+<!--        <i class="fa fa-refresh" ></i> assim que faz aparecer icones-->
+        <div id="Stream">
+            <div id="Nstream">
+                <h1>Steam</h1></div>
+            <img src="./twitch.jpg"/>
+        </div>
+
+    </div>
+
+
+</main>
+
+</body>
+
+</html>
