e085f95643dba2f39f54025423f4486f0135c040
EvolutecnetDev2 <83365151+EvolutecnetDev2@users.noreply.github.com>
2022-05-30 12:02:02 -0300

    Desenvolver aulas em php

---
+++ POO/exemplo01.php
@@ -0,0 +1,26 @@
+<?php
+//
+//class Pessoa {
+//
+//    public $nome; //Atributo
+//
+//    public function falar(){  //Método
+//        return "O meu nome é: ".$this->nome;
+//    }
+//
+//}
+//
+//$glaucio = new Pessoa();
+//$glaucio->nome = "<strong>Glaucio Daniel</strong>";
+//echo $glaucio->falar();
+//
+//
+//?>
+<?php
+
+class carro{
+    private $modelo;
+    private $motor;
+    private $ano;
+}
+?>
