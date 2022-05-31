1fc4bea4b611efc691acae45877bc0d45eb39314
EvolutecnetDev2 <83365151+EvolutecnetDev2@users.noreply.github.com>
2022-05-31 12:02:57 -0300

    Iniciar classe Carro

--- POO/exemplo01.php
+++ POO/exemplo01.php
@@ -16,11 +16,73 @@
 //
 //
 //?>
+<!--------------------------------------------------------------------------------------------------------------------->
+<?php
+//
+//class Carro{ //criando classe carro
+//
+//    private $modelo; // atributo
+//    private $motor; // atributo
+//    private $ano; // atributo
+//
+//                                          // metodos get e set
+//
+//    public function getModelo(){ //pega o valor
+//
+//        return $this->modelo;
+//    }
+//
+//    public function setModelo($modelo){ //define o valor
+//
+//        $this->modelo = $modelo;
+//
+//    }
+//
+//    public function getMotor(){ //pega o valor
+//
+//        return $this->motor;
+//
+//    }
+//
+//    public function setMotor($motor){ //define o valor
+//
+//        $this->motor = $motor;
+//
+//    }
+//
+//    public function getAno(){ //pega o valor
+//
+//        return $this->ano;
+//
+//    }
+//
+//    public function setAno($ano){ //define o valor
+//
+//        $this->ano = $ano;
+//
+//    }
+//
+//    public function exibir(){
+//
+//        return array(
+//            "modelo"=>$this->getModelo(),
+//            "motor"=>$this->getMotor(),
+//            "ano"=>$this->getAno()
+//        );
+//    }
+//}
+//
+//$gol = new Carro();
+//$gol->setModelo("Gol GT");
+//$gol->setMotor("1.6");
+//$gol->setAno("2017");
+//
+//print_r($gol->exibir());
+//
+//?>
+<!--------------------------------------------------------------------------------------------------------------------->
 <?php
 
-class carro{
-    private $modelo;
-    private $motor;
-    private $ano;
-}
+
+
 ?>
